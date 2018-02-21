193eff86d54da5ec17912f357040abc6aadda30f
CristhoferMF <cristhofermontalvo@gmail.com>
2018-02-21 12:26:15 -0500

    Avance de la pestaña de login ademas faltan cambiar rutas para el css,imagenes

--- view/login.php
+++ view/login.php
@@ -1,9 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Seguimiento | Login</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../assets/css/login.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 </head>
 <body>
-	<h1>Login</h1>
+   <div class="bg-blue remove_float">
+   </div>
+    <div class="content-mother">
+        <div class="content_width col-xs-11 remove_float center-block white border-radius-8">
+        <h2 class="text-center">Iniciar Sesion</h2>
+	        <form action="">
+            <div class="input-content">
+                <img src="../assets/img/user.png" alt="">
+                <input type="text" placeholder="Correo Electronico" class="input-content-input">
+            </div>
+            <div class="input-content small-top-space">
+                <img src="../assets/img/pass.png" alt="">
+                <input type="password" placeholder="Contraseña" class="input-content-input">
+            </div>     
+	        </form>
+   </div>
+    </div>    
 </body>
 </html>
